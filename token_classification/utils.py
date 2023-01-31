ebe8d6da1aef36603df208ffc1aafd197795409b
Lucy Havens <ljhavens@comcast.net>
2023-01-31 17:39:49 +0000

    Ran baseline token classifiers, beginning feature engineering with POS

--- token_classification/utils.py
+++ token_classification/utils.py
@@ -157,3 +157,23 @@ def getNumericLabels(target_data, labels_dict=labels):
             numeric_tuple = tuple((numeric_target))
             numeric_target_data += [numeric_tuple]
     return numeric_target_data
+
+
+def getPerformanceMetrics(y_test_binarized, predicted, matrix, classes, original_classes, no_to_label_dict):
+    tn = matrix[:, 0, 0]  # True negatives
+    fn = matrix[:, 1, 0]  # False negatives
+    tp = matrix[:, 1, 1]  # True positives
+    fp = matrix[:, 0, 1]  # False positives
+    class_names = [no_to_label_dict[c] for c in original_classes]
+    
+    [precision, recall, f_1, suport] = precision_recall_fscore_support(
+        y_test_binarized, predicted, beta=1.0, zero_division=0, labels=classes
+    )
+    
+    df = pd.DataFrame({
+        "labels":class_names, "true_neg":tn, "false_neg":fn, "true_pos":tp, "false_pos":fp,
+        "precision":precision, "recall":recall, "f_1":f_1
+    })
+    
+    return df
+

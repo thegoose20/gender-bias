8d8c47b77be3aa9b74dff3e82d91eb571d9a8cf8
Lucy Havens <ljhavens@comcast.net>
2023-02-09 11:23:34 +0000

    Removing conflicting token_classif files

--- token_classification/config.py
+++
@@ -1,5 +0,0 @@
-tokc_path = "../data/token_clf_data/"
-docc_path = "../data/doc_clf_data/"
-crc_meta_path = "../data/crc_metadata/"
-agg_path = "../data/aggregated_data/"
-s2v_reddit_path = "../../../../../../group/project/s1545703/gender-bias/data/s2v_old"

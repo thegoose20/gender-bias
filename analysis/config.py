34906a5b4a30c78a06a9dd764f4e0c9ad47220d1
Lucy Havens <ljhavens@comcast.net>
2023-10-25 00:17:17 +0100

    Removing incomplete and unnecessary work from repo

--- analysis/config.py
+++
@@ -1,5 +0,0 @@
-crc_meta_path = "../data/crc_metadata/"
-doc_path = crc_meta_path+"descriptions_brat/"
-agg_path = "../data/aggregated_data/"
-tokc_path = "../data/token_clf_data/"
-docc_path = "../data/doc_clf_data/"
